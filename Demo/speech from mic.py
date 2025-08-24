fa1bba5c3b74ba400158271bbb9aaa3f78148fc4
ArpitK0077 <arpitkh2005@gmail.com>
2025-08-24 21:58:28 +0530

    Initial Commit

---
+++ Demo/speech from mic.py
@@ -0,0 +1,13 @@
+# app/record.py
+import sounddevice as sd
+import soundfile as sf
+
+def record(filename="input.wav", duration=5, fs=16000):
+    print("🎙 Recording...")
+    audio = sd.rec(int(duration*fs), samplerate=fs, channels=1, dtype="int16")
+    sd.wait()
+    sf.write(filename, audio, fs)
+    print("✅ Saved:", filename)
+
+if __name__ == "__main__":
+    record()
